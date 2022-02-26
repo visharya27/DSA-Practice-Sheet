53e040027a864945b85a8b93cb66b6a944eff052
visharya27 <56781721+visharya27@users.noreply.github.com>
2022-02-26 15:02:09 +0530

    Update 1_Reverse_the_Array_or_String.cpp

--- 1_Reverse_the_Array_or_String.cpp
+++ 1_Reverse_the_Array_or_String.cpp
@@ -1,5 +1,5 @@
 //You are given a string s. You need to reverse the string.
-
+//Method - 1: Iterative method:
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -35,3 +35,37 @@ string reverseWord(string str){
 }
 //Time Complexity = O(n)
 //Space Complexity = O(n)
+
+
+//Method-2: Recursive Method:
+#include<iostream>
+using namespace std;
+
+void Reverse_Array(int arr[], int start, int end)
+{
+    if(start >= end)
+    {
+        return;
+    }
+    else
+    {
+            int temp;
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            return Reverse_Array(arr,start+1,end-1);
+    }
+
+}
+int main()
+{
+    int a[5] = {1,2,3,4,5};
+    Reverse_Array(a,0,4);
+    for(int i=0; i<5; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
+//Time Complexity: O(n)
+//Space Complexity: O(n)
